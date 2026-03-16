7c22db917921587bc9981d0d015eabb7c35e5eec
Magd El Zalameh <Magd El Zalameh>
2026-03-16 04:57:47 +0200

    cors

--- server.js
+++ server.js
@@ -16,27 +16,24 @@ const app = express();
 // Body parser
 app.use(express.json());
 
-app.use(cors());
-// // CORS setup
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'http://localhost:3001',
-//   'https://magdishere.github.io'
-// ];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://magdishere.github.io'
+];
 
-// app.use(
-//   cors({
-//     origin: function(origin, callback) {
-//       if (!origin) return callback(null, true); // allow Postman, etc.
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         const msg = `CORS policy: origin ${origin} not allowed`;
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//     credentials: true, // allow cookies
-//   })
-// );
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allows Postman, mobile apps
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error(`CORS policy: origin ${origin} not allowed`), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 // Set static folder for uploads
 const path = require('path');
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
